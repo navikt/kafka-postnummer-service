21f430d277650072217e16dd725dbe1ef9657ae5
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-09-22 09:21:09 +0200

    Deploy in after_success

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set -e
+set -ev
 
-make release
-git push --tags https://$GITHUB_TOKEN@github.com/navikt/kafka-postnummer-service HEAD:master
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    make release
+    git push --tags https://${GITHUB_TOKEN}@github.com/navikt/kafka-postnummer-service.git HEAD:master
+fi
