2221f3ec3844ab567fbe168b5cba486086e9e4ae
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-12-13 13:47:03 +0100

    Use commit sha as docker tag

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM navikt/java:8
 
-COPY build/install/kafka-postnummer/bin/kafka-postnummer bin/app
-COPY build/install/kafka-postnummer/lib ./lib/
+COPY build/libs/kafka-postnummer-all.jar app.jar
--- travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-    make release
-    git push --tags https://${GITHUB_TOKEN}@github.com/navikt/kafka-postnummer-service.git HEAD:master
-fi
